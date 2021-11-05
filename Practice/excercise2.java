0779dca4dee337f372018e5d23f7e866318deb1f
DaveatCor <condaveat@gmail.com>
2021-11-05 10:46:23 +0700

    finish practice, date 5/Nov/21

---
+++ Practice/excercise2.java
@@ -0,0 +1,55 @@
+import java.math.BigDecimal;
+import java.util.ArrayList;
+
+
+public class hello {
+	public static void main(String args[]) {
+		
+		// 4
+//		int price = 20 * 33;
+//        
+//		BigDecimal a = new BigDecimal(price);
+//		BigDecimal b = new BigDecimal("100");
+//		BigDecimal c = a.divide(b);
+//		
+//		System.out.println(c);
+		
+
+		// 5
+//		String[] strArr = new String[50];
+		
+		// 7
+//		ArrayList<String> strArr = new ArrayList<String>();
+//		
+//		for (int i = 0; i < 50; i++) {
+//			strArr.add("\\");
+//			System.out.println(strArr.get(i)+i);
+//		}
+		
+		// 8
+//		int[][] intArr = new int[3][4];
+		
+		
+		// 10
+//		int[][] arr = new int[3][4];
+//		
+//		for (int i = 0; i < 3; i++) {
+//			if (i == 0) {
+//
+//				System.out.print("1st row: ");
+//			} else if (i == 1) {
+//
+//				System.out.print("2nd row: ");
+//			} else if (i == 2) {
+//
+//				System.out.print("3rd row: ");
+//			}
+//
+//			for (int j = 0; j < 4; j++) {
+//				arr[i][j] = i * j;	
+//				System.out.print(arr[i][j]);
+//			}
+//			System.out.println();
+//		}
+    }
+}
