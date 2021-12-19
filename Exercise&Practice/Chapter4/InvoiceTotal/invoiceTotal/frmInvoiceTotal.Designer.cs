25426049792eedac90e3b9da264bd7ffa618b6ae
DaveatCor <condaveat@gmail.com>
2021-12-19 16:02:56 +0700

    finish exercise chapter 4, date 19/Dec/21

---
+++ Exercise&Practice/Chapter4/InvoiceTotal/invoiceTotal/frmInvoiceTotal.Designer.cs
@@ -0,0 +1,297 @@
+﻿namespace invoiceTotal
+{
+    partial class frmInvoiceTotal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.txtEnterSubTotal = new System.Windows.Forms.TextBox();
+            this.txtTotal = new System.Windows.Forms.TextBox();
+            this.txtDiscountAmount = new System.Windows.Forms.TextBox();
+            this.txtDiscountPercent = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtNumberOfInvoices = new System.Windows.Forms.TextBox();
+            this.txtTotalOfInvoice = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtInvoiceAverage = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.btnClearTotals = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtSubTotal = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(307, 215);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(103, 37);
+            this.btnCalculate.TabIndex = 2;
+            this.btnCalculate.Text = "&Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnExit.Location = new System.Drawing.Point(561, 215);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(89, 37);
+            this.btnExit.TabIndex = 4;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // txtEnterSubTotal
+            // 
+            this.txtEnterSubTotal.Location = new System.Drawing.Point(221, 29);
+            this.txtEnterSubTotal.Name = "txtEnterSubTotal";
+            this.txtEnterSubTotal.Size = new System.Drawing.Size(106, 20);
+            this.txtEnterSubTotal.TabIndex = 1;
+            // 
+            // txtTotal
+            // 
+            this.txtTotal.Enabled = false;
+            this.txtTotal.Location = new System.Drawing.Point(221, 166);
+            this.txtTotal.Name = "txtTotal";
+            this.txtTotal.ReadOnly = true;
+            this.txtTotal.Size = new System.Drawing.Size(106, 20);
+            this.txtTotal.TabIndex = 9;
+            this.txtTotal.TabStop = false;
+            this.txtTotal.TextChanged += new System.EventHandler(this.txtTotal_TextChanged);
+            // 
+            // txtDiscountAmount
+            // 
+            this.txtDiscountAmount.Enabled = false;
+            this.txtDiscountAmount.Location = new System.Drawing.Point(221, 131);
+            this.txtDiscountAmount.Name = "txtDiscountAmount";
+            this.txtDiscountAmount.ReadOnly = true;
+            this.txtDiscountAmount.Size = new System.Drawing.Size(106, 20);
+            this.txtDiscountAmount.TabIndex = 8;
+            this.txtDiscountAmount.TabStop = false;
+            // 
+            // txtDiscountPercent
+            // 
+            this.txtDiscountPercent.Enabled = false;
+            this.txtDiscountPercent.Location = new System.Drawing.Point(221, 95);
+            this.txtDiscountPercent.Name = "txtDiscountPercent";
+            this.txtDiscountPercent.ReadOnly = true;
+            this.txtDiscountPercent.Size = new System.Drawing.Size(106, 20);
+            this.txtDiscountPercent.TabIndex = 7;
+            this.txtDiscountPercent.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(43, 169);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Total";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(43, 134);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(88, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Discount Amount";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(43, 98);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Discount Percent:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(43, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "&Enter Subtotal:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(421, 74);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(102, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Number of Invoices:";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // txtNumberOfInvoices
+            // 
+            this.txtNumberOfInvoices.Enabled = false;
+            this.txtNumberOfInvoices.Location = new System.Drawing.Point(544, 71);
+            this.txtNumberOfInvoices.Name = "txtNumberOfInvoices";
+            this.txtNumberOfInvoices.ReadOnly = true;
+            this.txtNumberOfInvoices.Size = new System.Drawing.Size(106, 20);
+            this.txtNumberOfInvoices.TabIndex = 14;
+            this.txtNumberOfInvoices.TabStop = false;
+            // 
+            // txtTotalOfInvoice
+            // 
+            this.txtTotalOfInvoice.Enabled = false;
+            this.txtTotalOfInvoice.Location = new System.Drawing.Point(544, 117);
+            this.txtTotalOfInvoice.Name = "txtTotalOfInvoice";
+            this.txtTotalOfInvoice.ReadOnly = true;
+            this.txtTotalOfInvoice.Size = new System.Drawing.Size(106, 20);
+            this.txtTotalOfInvoice.TabIndex = 16;
+            this.txtTotalOfInvoice.TabStop = false;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(421, 120);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(89, 13);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Total of Invoices:";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // txtInvoiceAverage
+            // 
+            this.txtInvoiceAverage.Enabled = false;
+            this.txtInvoiceAverage.Location = new System.Drawing.Point(544, 162);
+            this.txtInvoiceAverage.Name = "txtInvoiceAverage";
+            this.txtInvoiceAverage.ReadOnly = true;
+            this.txtInvoiceAverage.Size = new System.Drawing.Size(106, 20);
+            this.txtInvoiceAverage.TabIndex = 18;
+            this.txtInvoiceAverage.TabStop = false;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(421, 165);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(87, 13);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "Invoice average:";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // btnClearTotals
+            // 
+            this.btnClearTotals.Location = new System.Drawing.Point(436, 215);
+            this.btnClearTotals.Name = "btnClearTotals";
+            this.btnClearTotals.Size = new System.Drawing.Size(103, 37);
+            this.btnClearTotals.TabIndex = 3;
+            this.btnClearTotals.Text = "Clear Totals";
+            this.btnClearTotals.UseVisualStyleBackColor = true;
+            this.btnClearTotals.Click += new System.EventHandler(this.btnClearTotals_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(43, 67);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(49, 13);
+            this.label8.TabIndex = 19;
+            this.label8.Text = "Subtotal:";
+            this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // txtSubTotal
+            // 
+            this.txtSubTotal.Location = new System.Drawing.Point(221, 62);
+            this.txtSubTotal.Name = "txtSubTotal";
+            this.txtSubTotal.ReadOnly = true;
+            this.txtSubTotal.Size = new System.Drawing.Size(106, 20);
+            this.txtSubTotal.TabIndex = 20;
+            // 
+            // frmInvoiceTotal
+            // 
+            this.AcceptButton = this.btnCalculate;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnExit;
+            this.ClientSize = new System.Drawing.Size(706, 278);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.txtSubTotal);
+            this.Controls.Add(this.btnClearTotals);
+            this.Controls.Add(this.txtInvoiceAverage);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtTotalOfInvoice);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtNumberOfInvoices);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtTotal);
+            this.Controls.Add(this.txtDiscountAmount);
+            this.Controls.Add(this.txtDiscountPercent);
+            this.Controls.Add(this.txtEnterSubTotal);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnCalculate);
+            this.Name = "frmInvoiceTotal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.TextBox txtEnterSubTotal;
+        private System.Windows.Forms.TextBox txtTotal;
+        private System.Windows.Forms.TextBox txtDiscountAmount;
+        private System.Windows.Forms.TextBox txtDiscountPercent;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtNumberOfInvoices;
+        private System.Windows.Forms.TextBox txtTotalOfInvoice;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtInvoiceAverage;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button btnClearTotals;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtSubTotal;
+    }
+}
+
---
+++ Exercise&Practice/Chapter4/InvoiceTotal/invoiceTotal/frmInvoiceTotal.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace invoiceTotal
+{
+    public partial class frmInvoiceTotal : Form
+    {
+        decimal subtotal;
+        int numOfInvoice = 0;
+        decimal totalOfInvoice;
+        public frmInvoiceTotal()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            /***************************************
+
+            * this method calculates the total 
+
+            * for an invoice depending on a 
+
+            * discount that's based on the subtotal
+
+            ****************************************/
+
+
+            if (txtEnterSubTotal.Text != "")
+            {
+                // get the subtotal amount from the Subtotal text box
+                decimal subtotal = Convert.ToDecimal(txtEnterSubTotal.Text);
+
+                // set the discountPercent variable based
+                // on the value of the subtotal variable
+                decimal discountPercent = 0m; // the m indicates a decimal value
+                if (subtotal >= 500)
+                {
+                    discountPercent = .2m;
+                } else if (subtotal >= 250 && subtotal < 500)
+                {
+                    discountPercent = .15m;
+                }
+                else if (subtotal >= 1000 && subtotal < 250)
+                {
+                    discountPercent = .1m;
+                }
+                // calculate and assign the values for the 
+                // discountAmount and invoiceTotal variables
+                decimal discountAmount = subtotal * discountPercent;
+                decimal invoiceTotal = subtotal - discountAmount;
+
+                // format the values and display them in their text boxes
+                txtSubTotal.Text = txtEnterSubTotal.Text;
+                txtDiscountPercent.Text = discountPercent.ToString("p1"); // percent format with 1 decimal place
+                txtDiscountAmount.Text = discountAmount.ToString("c"); // currency format
+                txtTotal.Text = invoiceTotal.ToString("c");
+
+                // move the focus to the Subtotal text box
+                txtEnterSubTotal.Focus();
+
+                // Calculate Number Of Invoice 
+                numOfInvoice++;
+                txtNumberOfInvoices.Text = numOfInvoice.ToString();
+
+                // Calculate Total Invoice
+                totalOfInvoice += invoiceTotal;
+                txtTotalOfInvoice.Text = totalOfInvoice.ToString("c2");
+
+                // Calculate Total Average
+                txtInvoiceAverage.Text = (totalOfInvoice / numOfInvoice).ToString("c2");
+            }
+        }
+
+        private void txtTotal_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnClearTotals_Click(object sender, EventArgs e)
+        {
+            numOfInvoice = 0;
+            totalOfInvoice = 0m;
+
+            // Left
+            txtEnterSubTotal.Text = "";
+            txtSubTotal.Text = "";
+            txtDiscountPercent.Text = "";
+            txtDiscountAmount.Text = "";
+            txtTotal.Text = "";
+
+            // Right
+            txtNumberOfInvoices.Text = "";
+            txtTotalOfInvoice.Text = "";
+            txtInvoiceAverage.Text = "";
+            txtEnterSubTotal.Focus();
+        }
+    }
+}
