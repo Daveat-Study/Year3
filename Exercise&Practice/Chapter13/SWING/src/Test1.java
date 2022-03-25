9423f4f92aee4827eb91a2a1167f9256ab032b53
DaveatCor <condaveat@gmail.com>
2022-03-25 09:58:25 +0700

    added image and set source path of res folder, date 25/Mar/22

--- Exercise&Practice/Chapter13/SWING/src/Test1.java
+++ Exercise&Practice/Chapter13/SWING/src/Test1.java
@@ -1,4 +1,6 @@
 import java.awt.*;
+
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 
 public class Test1 {
@@ -6,11 +8,15 @@ public class Test1 {
 	JFrame f;
 	
 	public Test1() {
+
 		f = new JFrame("Demo Swing");
 		f.setSize(400, 400);
 		f.setLayout(new FlowLayout());
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		f.getContentPane().setBackground(Color.red);
+		ImageIcon icon = new ImageIcon("/Users/mac/Desktop/icons8-loyalty-50.png");
+		ImageIcon icons = new ImageIcon(this.getClass().getResource("icon.png"));
+		f.setIconImage(icons.getImage());
 		f.setVisible(true);
 	}
 	
