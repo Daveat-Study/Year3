c8427590196dff97f56110b69c1479575a85614f
DaveatCor <condaveat@gmail.com>
2022-04-01 09:42:45 +0700

    checkout java, date 1/Apr/22

---
+++ Exercise&Practice/Chapter12/frmProductMain/frmProductMain/Product.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace frmProductMain
+{
+    public class Product
+    {
+        private string code;
+        private string description;
+        private decimal price;
+
+        public Product() { }
+        public Product(string code, string des, decimal price)
+        {
+            this.code = code;
+            this.description = des;
+            this.price = price;
+        }
+
+        public string getCode
+        {
+            get
+            {
+                return code;
+            }
+            set
+            {
+                code = value;
+            }
+        }
+
+        public string getDescription
+        {
+            get
+            {
+                return description;
+            }
+            set
+            {
+                description = value;
+            }
+        }
+
+        public decimal getPrice
+        {
+            get
+            {
+                return price;
+            }
+            set
+            {
+                price = value;
+            }
+        }
+
+        public string GetDisplayText(string sep)
+        {
+            return code + sep + price.ToString("c") + sep + description;
+        }
+    }
+}
--- Exercise&Practice/Chapter12/frmProductMain/frmProductMain/frmProductMain.cs
+++ Exercise&Practice/Chapter12/frmProductMain/frmProductMain/frmProductMain.cs
@@ -11,61 +11,7 @@ using System.Windows.Forms;
 namespace frmProductMain
 {
 
-    public class Product
-    {
-        private string code;
-        private string description;
-        private decimal price;
-
-        public Product() { }
-        public Product (string code, string des, decimal price)
-        {
-            this.code = code;
-            this.description = des;
-            this.price = price;
-        }
-
-        public string getCode
-        {
-            get
-            {
-                return code;
-            }
-            set
-            {
-                code = value;
-            }
-        }
-
-        public string getDescription
-        {
-            get
-            {
-                return description;
-            }
-            set
-            {
-                description = value;
-            }
-        }
-
-        public decimal getPrice
-        {
-            get
-            {
-                return price;
-            }
-            set
-            {
-                price = value;
-            }
-        }
-
-        public string GetDisplayText(string sep)
-        {
-            return code + sep + price.ToString("c") + sep + description;
-        }
-    } 
+    
     public partial class frmProductMain : Form
     {
         public frmProductMain()
